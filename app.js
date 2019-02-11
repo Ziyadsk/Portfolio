edd723d3f14bad67107eb78dc55db0964f94507a
Ziyadsk <ziyadkader@gmail.com>
2019-02-11 10:03:32 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+function displaySection(link,section){
+	
+	let l = document.querySelector(`${link}`); 
+	let s = document.querySelector(`${section}`);
+	document.querySelectorAll("nav a").forEach(function(e){  
+		e.classList.remove("active");
+	});
+	l.classList.add("active"); 
+	
+	document.querySelectorAll("main > div").forEach(function(e){
+		e.style.display = "none";
+	});
+	if(s === document.querySelector("#social-networks")){
+		animateLinks() ; 
+	}
+	if(s === document.querySelector("#skills")){
+		console.log("eheh 4e skill ba3d");
+		animateSkillSections() ; 
+	}
+	document.querySelector("nav").style.animation = "nav-element-pressed 1s forwards" ; 
+	s.style.display = "flex";
+	s.style.flexDirection = "column";     
+	s.style.animation = "section-to-up 0.5s forwards"; 
+}
+
+function displayDescriptionOnLinks(element){
+	let el = document.querySelector(`.${element}`);
+	document.querySelectorAll('#social-networks a').forEach((e) => {
+		e.classList.remove('link-hovered');
+	}); 
+	el.parentElement.classList.add("link-hovered");
+	el.onmouseleave = function(e) {
+		el.parentElement.classList.remove("link-hovered");
+	}
+}
+
+// to animate links in #social-networks; 
+function animateLinks(){
+	let interval = 100; 
+	document.querySelectorAll("#social-networks a").forEach((e,index) => {
+		setTimeout(function () {
+			e.style.animation = "display-link-animation 1s forwards";
+		},index*interval);
+		
+	});
+
+}
+
+// to animate skills sections ;
+
+function animateSkillSections(){
+	let interval = 100; 
+	document.querySelectorAll("#skills section").forEach((e,index) => {
+		setTimeout(function () {
+			e.style.animation = "display-skills-section-animation 0.5s forwards";
+		},index*interval);
+		
+	});
+
+}
+// skills 
+(function () {
+	document.querySelectorAll("#skills li").forEach((e) => {
+		e.onmouseover = function(x){
+			skillDescription = document.querySelector("#skill-description"); 
+			skillDescription.innerHTML = e.getAttribute("skill-description");
+			e.onmouseleave = function(x){
+				skillDescription.innerHTML = "";
+			}
+		}
+	});
+})() ; 
---
+++ php.php
@@ -0,0 +1,28 @@
+<?php
+function fruits($fruits){
+    var_dump($fruits);
+    // foreach($fruits as $f){
+    //     echo "$f <<";
+    // }
+}
+fruits("oranges","kiwi","apples");
+// interface Cool{
+    // function beCool() : string ;
+// }
+// class Person implements Cool {
+//     public function __construct($name,$age) {
+//         $this->name = $name ; 
+//         $this->age = $age ; 
+//     }
+//     public function beCool() : string {
+//         return "Hi My name is $this->name and i am $this->age years old" ; 
+//     }
+    
+// }
+// // 
+// $ziyad = new Person("Ziyad",28) ; 
+// echo $ziyad->beCool() ; 
+// 
+// 
+
+?>
