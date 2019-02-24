3478f773f2cc59c8b51866aacc41b842d78bd2ba
Ziyadsk <ziyadkader@gmail.com>
2019-02-24 03:13:31 +0000

    mobile support

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function displaySection(link,section){
 		e.classList.remove("active");
 	});
 	l.classList.add("active"); 
-	
 	document.querySelectorAll("main > div").forEach(function(e){
 		e.style.display = "none";
 	});
@@ -14,15 +13,25 @@ function displaySection(link,section){
 		animateLinks() ; 
 	}
 	if(s === document.querySelector("#skills")){
-		console.log("eheh 4e skill ba3d");
 		animateSkillSections() ; 
 	}
 	document.querySelector("nav").style.animation = "nav-element-pressed 1s forwards" ; 
+	
+	/* Make nav hide on mobile */
+	if(window.matchMedia("(max-width:700px)").matches){
+		document.querySelector("main").style.display = "flex";
+		document.querySelector("nav").style.display = "none";
+		s.style.display = "flex";
+		s.style.display.animateLinks = "section-to-up 0.5s forwards";
+		s.style.flexDirection = "column";
+		return 
+	}
 	s.style.display = "flex";
 	s.style.flexDirection = "column";     
 	s.style.animation = "section-to-up 0.5s forwards"; 
 }
 
+// display description on links for #social-network section
 function displayDescriptionOnLinks(element){
 	let el = document.querySelector(`.${element}`);
 	document.querySelectorAll('#social-networks a').forEach((e) => {
@@ -58,7 +67,6 @@ function animateSkillSections(){
 	});
 
 }
-
 // skills 
 (function () {
 	document.querySelectorAll("#skills li").forEach((e) => {
@@ -71,3 +79,9 @@ function animateSkillSections(){
 		}
 	});
 })() ; 
+
+function displayNavOnMobile(){
+	document.querySelector("nav").style.animation = "none";
+	document.querySelector("main").style.display = "none";
+	document.querySelector("nav").style.display = "flex";
+}
